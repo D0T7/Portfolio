024a2cc5982868be9651eb7ffb9a1c4738fa7461
Asim <uze761@gmail.com>
2023-04-11 10:47:48 +0530

    updated header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,7 +9,8 @@ const Header = () => {
       <div className="container header_container" id='Home'>
         <h5>Hello I'm</h5>
         <h1>Asim Ali Khan</h1>
-        <h5 className="text-light">I'm a Computer Science Student and a Machine Learning Enthusiast </h5>
+        <h5 className="text-light">I'm a Computer Science Student , a privacy advocate and a Machine Learning Enthusiast </h5>
+        <h5 className='text-light-2'>I'm currently interning at an AI startup called ConverseNow as an Automation Engineer.</h5>
         <CTA />
         <HeaderSocials />
 
